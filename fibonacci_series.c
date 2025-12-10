d05481e3a322d119ca49a1899379fc5d21216c91
Inderkang <inderpreet0855@gmail.com>
2025-12-10 16:45:58 +0530

    fibonacci

---
+++ fibonacci_series.c
@@ -0,0 +1,18 @@
+// inderpreet
+// to print fibonacci series
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, next;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    printf("\n");
+}
